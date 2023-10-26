919c8b6bf50965e67094fb9bb7e470035d076daf
mylonamar <sdi1900229@di.uoa.gr>
2023-10-26 14:52:00 +0300

    read input and print elements

--- src/library.cpp
+++ src/library.cpp
@@ -1,7 +1,46 @@
-#include "library.h"
-
+#include <cstdio>
+//#include <cstdint>
 #include <iostream>
+//#include <fstream>
 
-void hello() {
-    std::cout << "Hello, World!" << std::endl;
+
+using namespace std;
+
+
+uint32_t readElementsFromFile(const char* filename, float* elements) {
+    uint32_t N;
+
+    FILE *file = fopen(filename, "rb");
+
+    if (!file) {
+        fprintf(stderr, "Unable to open file\n");
+        return 0;
+    }
+
+    fread(&N, sizeof(uint32_t), 1, file);
+
+    fread(elements, sizeof(float), N * 100, file);
+
+
+
+    // Now you can use the elements array
+
+    fclose(file);
+    return N;
+}
+
+int main(){
+    float *elements = new float[100 * 100];
+    uint32_t N = readElementsFromFile("input1.bin",elements);
+
+    for (int i = 0; i < N; i++) {
+        cout << "Element " << i + 1 << ":\n";
+        for (int j = 0; j < 100; j++) {
+            cout << elements[i * 100 + j] << " " << endl;
+        }
+        cout << endl;
+    }
+
+    delete[] elements;
+
 }
