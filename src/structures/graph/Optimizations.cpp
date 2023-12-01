4c671d9132d737ee7aca055ab3d3c0c962733c81
mylonamar <sdi1900229@di.uoa.gr>
2023-12-01 22:53:47 +0200

    new graph

--- src/structures/graph/Optimizations.cpp
+++ src/structures/graph/Optimizations.cpp
@@ -20,6 +20,114 @@ int Optimizations::checkDuplicate(Vector<Neighbors> neighborsVector1, Vector<Nei
     return 0;
 }
 
+int Optimizations::checkRandomNum(Vector<neighborList> neighborListArg, int randNum, int currentPointId) {
+    int flag = 0;
+
+    for (int p = 0; p < neighborList.getSize(); p++) {
+        //if num already in the neighbor list
+        if (neighborList.at(p).neighbor.getId() == randNum) {
+            flag = 1;
+            break;
+        }
+
+    }
+    //find reverse neighbor vector from point(currentPoint)
+    Vector<neighborList> neighborCheckList;
+    Point reverseCheckPoint = this->points.at(randNum - 1);
+    this->graph.find(reverseCheckPoint, neighborCheckList);
+
+    //if reverse neighbor list is full
+    if (neighborCheckList.getSize() == this->K) {
+        flag = 1;
+    }
+
+    return flag;
+}
+
+
+void Optimizations::setKRandomNeighbors() {
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    //for every point
+    for (int i = 0; i < this->numOfPoints; i++) {
+        int randomNum;
+        Vector<neighborList> neighborListArg;
+        Point currentPoint = this->points.at(i);
+        this->graph.find(currentPoint, neighborListArg);
+
+        int neighborCapacity = this->K - neighborListArg.getSize();
+        //for every neighbor
+        for (int j = 0; j < neighborCapacity; j++) {
+            int loopFlag = 1;
+            int count = 0;
+            while (loopFlag) {
+                //generate random num
+
+                randomNum = (rand() % (this->numOfPoints)) + 1;
+                while (currentPoint.getId() == randomNum) {
+                    randomNum = (rand() % (this->numOfPoints)) + 1;
+
+                }
+
+                //if num is ok, stop the loop
+                loopFlag = checkRandomNum(neighborListArg, randomNum, i + 1);
+
+                count++;
+                if (count == 20) {
+
+                    char buffer1[100]; // Adjust the buffer size as needed
+                    char buffer2[100]; // Adjust the buffer size as needed
+
+                    sprintf(buffer1, "cannot find a matching point!");
+                    sprintf(buffer2, "point with id: %d do not correspond to other neighbors!", i + 1);
+                    LOG_WARN(buffer1);
+                    LOG_WARN(buffer2);
+
+                    loopFlag = 0;
+                }
+
+            }
+
+            for (int p = 0; p < this->points.getSize(); p++) {
+                //find neighbor from points
+                if (this->points.at(p).getId() == randomNum) {
+                    float distance;
+                    if(strcmp(this->metrics, "manhattan") == 0) {
+                        distance = Metrics::manhattanDistance(currentPoint.getCoordinates(),
+                                                              this->points.at(p).getCoordinates(), this->dimensions);
+
+                    }
+                    else{
+                        distance = Metrics::euclideanDistance(currentPoint.getCoordinates(),
+                                                              this->points.at(p).getCoordinates(), this->dimensions);
+                    }
+
+                    //init current point neighbor
+                    Neighbors neighborsOfPoint(randomNum, distance, this->points.at(p).getCoordinates());
+                    neighborsVector.push_back(neighborsOfPoint);
+
+                    //find reverse neighbor from point(currentPoint)
+                    Vector<Neighbors> reverseNeighborsVector;
+                    Point reversePoint = this->points.at(randomNum - 1);
+                    this->graph.find(reversePoint, reverseNeighborsVector);
+
+                    //init neighbor reserve neighbor
+                    Neighbors reverseNeighborsOfPoint(i + 1, distance, currentPoint.getCoordinates());
+                    reverseNeighborsVector.push_back(reverseNeighborsOfPoint);
+
+                    //put neighbor reverse neighbor
+                    this->graph.insert(reversePoint, reverseNeighborsVector);
+                    break;
+                }
+            }
+
+        }
+        this->graph.insert(currentPoint, neighborsVector);
+
+    }
+
+}
+
 int Optimizations::localJoin() {
     int flag = 0;
 
